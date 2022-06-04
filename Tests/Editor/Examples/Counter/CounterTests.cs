fc579befe676396423fc6e2e0bea6f7d75aeded4
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-04 01:11:39 -0300

    feat: add example usage

---
+++ Tests/Editor/Examples/Counter/CounterTests.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+
+namespace CoroutineSubstitute.Examples
+{
+    public class SubstitutionTests
+    {
+        class BaseSubstitutionTests
+        {
+            public ICoroutineRunner Runner { get; private set; }
+
+            public Counter Counter { get; private set; }
+
+            [SetUp]
+            public void Setup ()
+            {
+                Runner = CoroutineSubstitute.Create();
+                Counter = new Counter(Runner);
+            }
+        }
+
+        class StartCounterTests : BaseSubstitutionTests
+        {
+            [Test]
+            public void Starts_At_Zero ()
+            {
+                Counter.StartCounter();
+                Assert.AreEqual(0, Counter.Current);
+            }
+
+            [Test]
+            public void MoveNext_Increments_To_One ()
+            {
+                Counter.StartCounter();
+                Runner.MoveNext();
+                Assert.AreEqual(1, Counter.Current);
+            }
+
+            [Test]
+            public void Two_MoveNext_Increments_To_Two ()
+            {
+                Counter.StartCounter();
+
+                Runner.MoveNext();
+                Runner.MoveNext();
+
+                Assert.AreEqual(2, Counter.Current);
+            }
+
+            [Test]
+            public void Three_MoveNext_Increments_To_Three ()
+            {
+                Counter.StartCounter();
+
+                Runner.MoveNext();
+                Runner.MoveNext();
+
+                Assert.AreEqual(2, Counter.Current);
+            }
+        }
+    }
+}
---
+++ Tests/Editor/Examples/Counter/TestObjects/Counter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+
+namespace CoroutineSubstitute.Examples
+{
+    public class Counter
+    {
+        public int Current { get; private set; }
+
+        ICoroutineRunner runner;
+
+        public Counter (ICoroutineRunner runner)
+        {
+            this.runner = runner;
+        }
+
+        public void StartCounter ()
+        {
+            runner.StartCoroutine(CounterRoutine());
+        }
+
+        IEnumerator CounterRoutine ()
+        {
+            while (true)
+            {
+                Current++;
+                yield return null;
+            }
+        }
+    }
+}
