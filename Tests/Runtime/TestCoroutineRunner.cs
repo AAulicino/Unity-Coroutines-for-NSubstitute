ebc343b144b6fc7ab77426392af0d5f3e0b181dc
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-04 02:01:39 -0300

    refactor: organize tests

---
+++ Tests/Runtime/TestCoroutineRunner.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using CoroutineSubstitute;
+using UnityEngine;
+
+public class TestCoroutineRunner : MonoBehaviour, ICoroutineRunner
+{
+}
