68c7ff35147898e89f90b3a0dc9e9b60303da3d8
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-17 02:06:28 -0300

    ci: ci fixes

--- .github/scripts/create_unity_project.sh
+++ .github/scripts/create_unity_project.sh
@@ -4,6 +4,5 @@ set -x
 set -e
 
 mkdir Assets
-mkdir -p Packages/${{ github.event.repository.name }}
-mv -v $(git ls-tree --full-tree --name-only HEAD) $PROJECT_ROOT
+mkdir Packages
 echo $(python3 $SCRIPTS_DIR/setup_unity_package_manifest.py $PACKAGE_MANIFEST) >>Packages/manifest.json
--- .github/scripts/setup_unity_package_manifest.py
+++ .github/scripts/setup_unity_package_manifest.py
@@ -4,15 +4,15 @@ import os
 import sys
 import json
 
-manifestPath = sys.argv[1]
+packageManifestPath = sys.argv[1]
 
-with open(manifestPath) as file:
+with open(packageManifestPath) as file:
     packageManifest = json.load(file)
 
 manifest = {
     "dependencies": {
         "com.aaulicino.nsubstitute": "https://github.com/AAulicino/Unity3D-NSubstitute.git",
-        f"{packageManifest['name']}": f"file:/{os.path.dirname(manifestPath)}",
+        f"{packageManifest['name']}": f"file:/{os.path.dirname(packageManifestPath)}",
         "com.unity.test-framework": "1.1.31",
         "com.unity.testtools.codecoverage": "1.1.1"
     },
