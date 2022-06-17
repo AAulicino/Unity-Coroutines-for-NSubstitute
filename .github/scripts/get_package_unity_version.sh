3a70c7845e2ecb7f11d6d92dce6b106ad53e6cb2
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-17 04:34:54 -0300

    ci: pass values to GITHUB_ENV

--- .github/scripts/get_package_unity_version.sh
+++ .github/scripts/get_package_unity_version.sh
@@ -6,4 +6,4 @@ set -x
 PACKAGE_MIN_VERSION=$(cat $1 | grep -oE '"unity":\s*"([^"]*)' | awk -F '"' '{print $4}')
 
 # get the latest unity patch from major.minor present in package manifest.
-echo UNITY_VERSION=$(unity-hub editors --releases | grep $PACKAGE_MIN_VERSION) >>prepare.env
+echo UNITY_VERSION=$(unity-hub editors --releases | grep $PACKAGE_MIN_VERSION) >>GITHUB_ENV.env
