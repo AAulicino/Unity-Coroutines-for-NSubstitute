6e4a78c823f3c22dc89e703e056593453ee977c0
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-19 17:43:25 -0300

    ci: add debug steps

--- .github/scripts/create_unity_manifest.py
+++ .github/scripts/create_unity_manifest.py
@@ -5,14 +5,14 @@ import sys
 import json
 
 packageName = sys.argv[1]
-tgzName = sys.argv[2]
+packageFile = sys.argv[2]
 
 os.mkdir("Packages")
 
 manifest = {
     "dependencies": {
         "com.aaulicino.nsubstitute": "https://github.com/AAulicino/Unity3D-NSubstitute.git",
-        f"{packageName}": f"file:../{tgzName}",
+        f"{packageName}": f"file:../{packageFile}",
         "com.unity.test-framework": "1.1.31",
         "com.unity.testtools.codecoverage": "1.1.1",
     },
