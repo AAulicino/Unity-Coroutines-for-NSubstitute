90ed84cb38673b7811ed10b6d57e3c76faf6ee1e
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-19 17:36:29 -0300

    ci: test from package tgz

--- .github/scripts/create_unity_manifest.py
+++ .github/scripts/create_unity_manifest.py
@@ -4,26 +4,23 @@ import os
 import sys
 import json
 
-packageManifestPath = sys.argv[1]
+packageName = sys.argv[1]
+tgzName = sys.argv[2]
 
-os.mkdir("Assets")
 os.mkdir("Packages")
-
-with open(packageManifestPath) as file:
-    packageManifest = json.load(file)
 
 manifest = {
     "dependencies": {
         "com.aaulicino.nsubstitute": "https://github.com/AAulicino/Unity3D-NSubstitute.git",
-        f"{packageManifest['name']}": f"file:/{os.path.dirname(packageManifestPath)}",
+        f"{packageName}": f"file:../{tgzName}",
         "com.unity.test-framework": "1.1.31",
         "com.unity.testtools.codecoverage": "1.1.1",
     },
-    "testables": [packageManifest["name"]],
+    "testables": [packageName],
 }
 
 print("Generated Manifest:")
-print(manifest)
+print(json.dumps(manifest, indent=4))
 
 with open("Packages/manifest.json", "w") as file:
     json.dump(manifest, file, ensure_ascii=False, indent=4)
