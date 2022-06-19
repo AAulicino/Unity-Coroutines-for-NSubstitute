6b2bc7fc02fe297eefeb086cdd9be05a413cad3e
Arthur Aulicino <arthur.aulicino@pixodust.com>
2022-06-19 18:00:20 -0300

    ci: fixes

--- .github/scripts/create_unity_manifest.py
+++ .github/scripts/create_unity_manifest.py
@@ -4,6 +4,8 @@ import os
 import sys
 import json
 
+print("Generating Unity manifest...")
+
 packageName = sys.argv[1]
 packageFile = sys.argv[2]
 
@@ -12,7 +14,7 @@ os.mkdir("Packages")
 manifest = {
     "dependencies": {
         "com.aaulicino.nsubstitute": "https://github.com/AAulicino/Unity3D-NSubstitute.git",
-        f"{packageName}": f"file:../{packageFile}",
+        packageName: f"file:../{packageFile}",
         "com.unity.test-framework": "1.1.31",
         "com.unity.testtools.codecoverage": "1.1.1",
     },
